882cd7b99f795dfa2838ed11dd71f67927d59037
Rohit Tayal <72745249+rohittayal96@users.noreply.github.com>
2020-10-18 20:23:21 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,33 +19,8 @@ if ARGV[0].empty?
 end
 
 message = ARGV[0]
-check_duplicate_msg = ARGV[1]
 repo = event["repository"]["full_name"]
 
-if ENV.fetch("GITHUB_EVENT_NAME") == "pull_request"
-  pr_number = event["number"]
-else
-  pulls = github.pull_requests(repo, state: "open")
-
-  push_head = event["after"]
-  pr = pulls.find { |pr| pr["head"]["sha"] == push_head }
-
-  if !pr
-    puts "Couldn't find an open pull request for branch with head at #{push_head}."
-    exit(1)
-  end
-  pr_number = pr["number"]
-end
-
-coms = github.issue_comments(repo, pr_number)
-
-if check_duplicate_msg == "true"
-  duplicate = coms.find { |c| c["body"] == message }
-
-  if duplicate
-    puts "The PR already contains this message"
-    exit(0)
-  end
-end
+pr_number = event["number"]
 
 github.add_comment(repo, pr_number, message)
